71dcc614cf5cb3455f93696f875c3c43eeec6278
Mateus Nadal <Mateus Nadal>
2021-07-07 23:31:49 -0300

    teste01

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace FundamentosCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
         }
     }
 }
