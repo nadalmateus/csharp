a195fdf097f4a93fefbf828ade3fe63e8bf4afc8
Mateus Nadal <nadalmateus@hotmail.com>
2021-10-19 21:34:22 -0300

    docs(namespaces): what's a namespace

---
+++ 1_namespaces/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1_namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = "Hello World";
+            Console.WriteLine(test);
+        }
+    }
+}
