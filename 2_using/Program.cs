6908c3faddd4649c509558523bca02703e48703c
Mateus Nadal <nadalmateus@hotmail.com>
2021-11-06 23:04:20 -0300

    feat: usings

---
+++ 2_using/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2_using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //importações definem as bibliotecas que nosso programa vai usar
+        }
+    }
+}
