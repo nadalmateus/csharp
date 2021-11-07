33f4a29027fb9978f0e153274e52fb8c2a27e4ab
Mateus Nadal <nadalmateus@hotmail.com>
2021-11-06 23:47:37 -0300

    docs: constantes

---
+++ 4_constantes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _4_constantes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int IDADE_MINIMA = 20;
+
+            Console.WriteLine(IDADE_MINIMA);
+
+        }
+    }
+}
